c1e2aa3b08417b43f2c6933dac5d2881de60babe
Maxamed-Maxamed <20088349@mail.wit.ie>
2024-09-11 18:57:57 +0100

    Added react-switch dependency and updated App.js to use new Route syntax

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-// src/App.js
 import React from 'react';
 import Header from './components/Header/Header';
 import Hero from './components/HeroSection/Hero';
 import Footer from './components/footer/Footer'
-// import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Services from './pages/Services';
 import Contact from './pages/Contact';
@@ -11,19 +9,15 @@ import Contact from './pages/Contact';
 function App() {
   return (
     <Router>
-    <div className="App">
-       <Header />
-      <Routes>
-
-     
-       <Route path="/" exact component={Hero} />
-       <Route path="/services" component={Services} />
-       <Route path="/contact" component={Contact} />
-
-      </Routes>
-       <Hero />
-       <Footer />
-    </div>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   );
 }
