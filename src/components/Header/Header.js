27e55bd547070f60da4f46da1a7fef28de7c9fc4
Maxamed-Maxamed <20088349@mail.wit.ie>
2024-09-11 19:08:28 +0100

    Updated Header component to use NavLink from react-router-dom, and added Contact page with form functionality

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 // src/components/Header.js
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
   return (
@@ -7,9 +8,9 @@ function Header() {
       <h1 className="text-white text-2xl font-bold">JLuxe Nails</h1>
       <nav>
         <ul className="flex space-x-4">
-          <li><a href="#home" className="text-white hover:text-gray-300">Home</a></li>
-          <li><a href="#services" className="text-white hover:text-gray-300">Services</a></li>
-          <li><a href="#contact" className="text-white hover:text-gray-300">Contact</a></li> 
+          <li><NavLink exact to="/" activeClassName="underline" className="text-white hover:text-gray-300">Home</NavLink></li>
+          <li><NavLink to="/services" activeClassName="underline" className="text-white hover:text-gray-300">Services</NavLink></li>
+          <li><NavLink to="/contact" activeClassName="underline" className="text-white hover:text-gray-300">Contact</NavLink></li>
         </ul>
       </nav>
     </header>
