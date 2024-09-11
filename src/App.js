80364c77ed1e5f4b7a496ad4e0d4568b8a66b3ad
Maxamed-Maxamed <20088349@mail.wit.ie>
2024-09-11 15:34:17 +0100

    Added Hero component to App.js and removed no changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 // src/App.js
 import React from 'react';
- import Header from './components/Header/Header';
+import Header from './components/Header/Header';
+import Hero from './components/HeroSection/Hero';
 
 function App() {
   return (
     <div className="App">
        <Header />
-      <h1 className="text-4xl text-center mt-10">Welcome to JLuxe Nails</h1>
+       <h1 className="text-4xl text-center mt-10">Welcome to JLuxe Nails</h1>
+       <Hero />
+       
+      
     </div>
   );
 }
