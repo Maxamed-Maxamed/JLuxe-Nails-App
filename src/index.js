d629e7f4ea2ac11c5a8bbd0cf9406ddcdcff0078
Maxamed-Maxamed <20088349@mail.wit.ie>
2024-09-11 17:52:56 +0100

    Configure Webpack and Tailwind CSS integration
    - Add CSS loaders to webpack.config.js
    - Create postcss.config.js for Tailwind processing
    - Update tailwind.config.js with custom breakpoints
    - Resolve CSS processing errors in build pipeline

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import '..src/style/index.css';
 import './style/index.css'
-import './style/App.css'
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
