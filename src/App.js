bd7153956fadc755b07bc53bf6e5aa0a5a6246ad
Maxamed-Maxamed <20088349@mail.wit.ie>
2024-09-11 15:27:53 +0100

    Added Header component to App.js and updated imports in index.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 // src/App.js
 import React from 'react';
+ import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+       <Header />
       <h1 className="text-4xl text-center mt-10">Welcome to JLuxe Nails</h1>
     </div>
   );
