a46b1893c68d573ed99a2a4c2e4010c4ae245547
Maxamed-Maxamed <20088349@mail.wit.ie>
2024-09-11 15:52:02 +0100

    Updated package-lock.json and package.json to include @babel/plugin-proposal-private-property-in-object, and modified Hero.js to change the backgroundImage URL.

--- src/components/HeroSection/Hero.js
+++ src/components/HeroSection/Hero.js
@@ -1,12 +1,17 @@
 // src/components/Hero.js
 import React from 'react';
 
+
 function Hero() {
   return (
     <section
       id="home"
       className="bg-cover bg-center h-96 flex flex-col justify-center items-center"
-      style={{ backgroundImage: 'url("https://example.com/your-image.jpg")' }}
+    //    style={{ backgroundImage: 'url("https://example.com/your-image.jpg")' }}
+    // style={{ backgroundImage: 'url("./assets/img/Designer.png")' }}
+    style={{ backgroundImage: 'url("/assets/img/Designer.png")' }}
+
+
     >
       <h1 className="text-white text-5xl font-extrabold">Welcome to JLuxe Nails</h1>
       <p className="text-white text-xl mt-4">Luxury Nails, Makeup, and Eyelashes at Your Fingertips</p>
