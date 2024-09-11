d48e9d35862427fc7647999f07523c2f54695eca
Maxamed-Maxamed <20088349@mail.wit.ie>
2024-09-11 18:32:41 +0100

    Added react-router-dom dependency and implemented routing in App.js

--- src/App.js
+++ src/App.js
@@ -3,30 +3,25 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Hero from './components/HeroSection/Hero';
 import Footer from './components/footer/Footer'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
 
 function App() {
   return (
+    <Router>
     <div className="App">
        <Header />
-       {/* <h1 className="text-4xl text-center mt-10">Welcome to JLuxe Nails</h1> */}
+       <Switch> 
+       <Route path="/" exact component={Hero} />
+       <Route path="/services" component={Services} />
+       <Route path="/contact" component={Contact} />
+
+       </Switch>
        <Hero />
-
-
-
-
-
-
-
-
-
-
-
-
-       
        <Footer />
-       
-      
     </div>
+    </Router>
   );
 }
 
