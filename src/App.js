4c2b95cf57263898abe873b79220bf76d8790b82
Maxamed-Maxamed <20088349@mail.wit.ie>
2024-09-16 18:29:05 +0100

    Added react-scroll dependency, updated package-lock.json and package.json, and refactored App.js and Header.js components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,3 @@
-// import React from 'react';
-// import Header from './components/Header/Header';
-// import Hero from './components/HeroSection/Hero';
-// import Footer from './components/footer/Footer'
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Services from './pages/Services';
-// import Contact from './pages/Contact';
-// import FeaturedServices from './components/FeaturedServices/FeaturedServices'
-// import Testimonials  from './components/Testimonials/Testimonials'
-// import AboutUs from './components/aboutUs/AboutUs';
-// import NewsletterSignup from './components/NewsLetter/NewsletterSignup';
-// function App() {
-//   return (
-//     <Router>
-//       <div className="App">
-//         <Header />
-//         <Routes>
-//           <Route path="/" element={<Hero />} />
-//           <Route path="/services" element={<Services />} />
-//           <Route path="/contact" element={<Contact />} />
-//           <FeaturedServices />  {/* Add FeaturedServices here */}
-//           <Testimonials> </Testimonials>
-//           <AboutUs></AboutUs> 
-//           <NewsletterSignup> </NewsletterSignup>
-
-//         </Routes>
-//         <Footer />
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import Header from './components/Header/Header';
 import Hero from './components/HeroSection/Hero';
